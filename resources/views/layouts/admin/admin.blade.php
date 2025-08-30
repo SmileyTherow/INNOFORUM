bebf235e8b0b185b034d676208c49de652a397be
Ahmad Zidan <santinodantonio1902@email.com>
2025-08-30 19:53:46 +0700

    Foto

--- resources/views/layouts/admin/admin.blade.php
+++ resources/views/layouts/admin/admin.blade.php
@@ -130,7 +130,7 @@
                 <div class="container-fluid px-4">
                     <div class="d-flex align-items-center justify-content-between small">
                         <div class="text-muted">Copyright &copy; {{ date('Y') }} Innoforum
-                            
+
                         </div>
                         <div>
                             <a href="#">Privacy Policy</a>
